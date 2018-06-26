bb89f313a38d9b15d8367a40d27c0dcedb4a0d1e
Vu Phan <vuphan365@gmail.com>
2018-06-26 16:36:25 +0700

    Test data Server receive

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -42,7 +42,7 @@ function router(sql) {
         } else {
           debug('Unexist');
           const msg = 'Sai tên đăng nhập hoặc mật khẩu';
-          const error = { msg };
+          const error = { msg, username, password };
           res.json(error);
         }
       });
